3b60cd39c6707c17870ae63f9686f0d1d2a8a6ee
ajobeili <ali.jbaily90@gmail.com>
2016-12-06 13:15:33 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,61 @@
-var gulp = require("gulp"),
-	concat = require("gulp-concat"),
-	uglify = require('gulp-uglify'),
-    cssmin= require("gulp-cssmin");
+var gulp       = require('gulp');
+var concat     = require('gulp-concat');
+var autoReload = require('gulp-auto-reload');
+var gutil      = require('gulp-util');
+// some paths to build from 
+var paths = {
+  html: "src/**/*.html",
+  css: "src/**/*.css"
+};
+ 
+// the output 
+var outapp = "dist";
+ 
+// this transformer is used to inject <script> 
+// tags into the html when reloader is enabled 
+var htmlInject = function() {
+  return gutil.noop();
+};
+ 
+gulp.task('html', function() {
+  gulp.src(paths.html)
+    .pipe(htmlInject())      // inject <script> 
+    .pipe(gulp.dest(outapp));
+});
+ 
+gulp.task('css', function() {
+  gulp.src(paths.css)
+    .pipe(concat('ui/app.css'))
+    .pipe(gulp.dest(outapp));
+});
+ 
+gulp.task('reloader', function() {
+  // start a server for reloading 
+  var reloader = autoReload();
+  // copy the auto-reload.js script to 
+  // the output 
+  reloader.script()
+    .pipe(gulp.dest(outapp));
+  // inject the script into html pages 
+  htmlInject = reloader.inject;
+  // start watching the output for changes 
+  gulp.watch(outapp + "/**/*", reloader.onChange);
+});
+ 
+// building html/css causes no <script> injection 
+gulp.task('default', ['html', 'css']);
+ 
+// 'reloader' sets htmlInject, and 'html' task will 
+// make injected html page 
+gulp.task('watch', ['reloader', 'html'], function() {
+  // watch all source for rebuild 
+  gulp.watch('./src/**/*', ['default']);
+});
 
 
-
-
-
-
-	gulp.task('concat', function(){
-		gulp.src([
-			"node_modules/bootstrap/dist/js/bootstrap.min.js"
-			])
-		
-		.pipe(concat("bundle.js"))
-		.pipe(gulp.dest("weather-dashboard/js"));
-
-
-
-	});
-
-	gulp.task("less", function(){
-		gulp.src("node_modules/bootstrap/less/bootstrap.less")
-		.pipe(less())
-		.pipe(concat("bundle.css"))
-		.pipe(gulp.dest("assests/css"));	
-
-	});
-
-	
-
-	gulp.task("cssmin", function(){
-		gulp.src("css/weather.css")
-		.pipe(cssmin())
-		.pipe(concat("weather.min.css"))
-		.pipe(gulp.dest("D:/ISS/Scratch/weather-dashboard/css"));
-	});
-
-		gulp.task("cssmin1", function(){
-		gulp.src("css/weather-icons.css")
-		.pipe(cssmin())
-		.pipe(concat("weather-icons.min.css"))
-		.pipe(gulp.dest("D:/ISS/Scratch/weather-dashboard/css"));
-	});
-
-
-		 gulp.task("cssmin2", function(){
-		 gulp.src("css/bootstrap.css")
-		.pipe(cssmin())
-		.pipe(concat("Bootstrap.min.css"))
-		.pipe(gulp.dest("D:/ISS/Scratch/weather-dashboard/css"));
-	});
+gulp.task('scripts', function() {
+  gulp.src('src/**/*.js')
+    .pipe(concat('script.js'))
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(gulp.dest('dist/'));
+});
